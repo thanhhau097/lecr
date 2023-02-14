30d191f7b2f42f4b1f3ff7bbe705081ea14f570f
nhannguyen2709 <nhanthanhnguyen94@gmail.com>
2023-02-14 17:33:36 +0000

    fix

--- losses.py
+++ losses.py
@@ -101,10 +101,9 @@ def cos_sim(a: Tensor, b: Tensor):
     if len(b.shape) == 1:
         b = b.unsqueeze(0)
 
-    # a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
-    # b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
-    # return torch.mm(a_norm, b_norm.transpose(0, 1))
-    return torch.mm(a, b.transpose(0, 1))
+    a_norm = torch.nn.functional.normalize(a, p=2, dim=1)
+    b_norm = torch.nn.functional.normalize(b, p=2, dim=1)
+    return torch.mm(a_norm, b_norm.transpose(0, 1))
 
 
 class MultipleNegativesRankingLoss(nn.Module):
